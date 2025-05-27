f4c947b613c5bf92ea9a116faed594945712b504
Ирина Барман <attame@yandex.ru>
2025-05-27 17:14:11 +0500

    добавь связь Purchase - Transaction

--- onfine/models/transactions.py
+++ onfine/models/transactions.py
@@ -22,13 +22,24 @@ class Transaction(db.Model):
     __tablename__ = "transactions"
 
     id = db.Column(db.BigInteger, primary_key=True)
-    user_id = db.Column(db.BigInteger, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(
+        db.BigInteger, db.ForeignKey("users.id"), nullable=False
+    )
 
     type = db.Column(db.Enum(TxType), nullable=False)
-    status = db.Column(db.Enum(TxStatus), nullable=False, default=TxStatus.pending)
+    status = db.Column(
+        db.Enum(TxStatus), nullable=False, default=TxStatus.pending
+    )
     network = db.Column(db.String(8))
     amount = db.Column(db.Numeric(18, 2), nullable=False)  # чистая сумма
     fee = db.Column(db.Numeric(18, 2))  # комиссия сети (withdraw)
     address = db.Column(db.String(128))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     confirmed_at = db.Column(db.DateTime)
+    # связь с Purchase
+    purchase_id = db.Column(
+        db.Integer, db.ForeignKey("purchases.id"), unique=True, nullable=True
+    )
+    purchase = db.relationship(
+        "Purchase", back_populates="transaction", uselist=False
+    )
